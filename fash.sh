8b8f6a7bf4c88c05b8cb1bfdc4c28282a6f515a2
FiqraID <51190920+FiqraID@users.noreply.github.com>
2019-06-25 11:57:14 +0700

    Add files via upload

---
+++ fash.sh
@@ -0,0 +1,47 @@
+#usr/system/eror
+#fuck
+
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+clear
+echo $bi "SELAMAT DATANG"
+sleep 1
+clear
+echo $ij "SELAMAT DATANG"
+sleep 1
+clear
+echo $or "SELAMAT DATANG"
+sleep 1
+clear
+echo $me "SELAMAT DATANG"
+sleep 1
+clear
+echo $ku "SELAMAT DATANG"
+sleep 1
+clear
+echo $pr "SELAMAT DATANG"
+sleep 1
+clear
+echo $cy "SELAMAT DATANG"
+sleep 1
+clear
+cowsay -f ghostbusters "BlackMaskCyberTeam" | lolcat
+echo $bi "+++++++++++++++++++++++++++++++++++++++++"
+echo "       TOOLS MEMPERCEPAT JARINGAN        " | lolcat
+
+echo "Author   : Fiqra ID" | lolcat
+echo "team     : B-MCT (BlackMaskCyberTeam)" | lolcat
+echo "Fb       : Fiqra ID" | lolcat
+echo $bi "+++++++++++++++++++++++++++++++++++++++++"
+sleep 2
+echo $or "MULAI MEMPERCEPAT..."
+sleep 3
+echo $pr "TUNGGU SEBENTAR......"
+sleep 5
+ping 8.8.8.8
